f7c07d54ec17c179771d2cdec997ec42877b864b
AriaAugs <nataug62@gmail.com>
2024-09-25 14:45:53 -0400

    completed update code and added docstrings

--- pyparticles/engine/sim.py
+++ pyparticles/engine/sim.py
@@ -1,4 +1,5 @@
 import pygame
+from pyparticles.engine import utils
 
 class ParticleSim():
     def __init__(self, sim_size, cell_size, bg_img=None, bg_clr=None):
@@ -12,8 +13,21 @@ class ParticleSim():
         self._cell_width, self._cell_height = cell_size
         sim_size = (self._sim_width*self._cell_width, self._sim_height*self._cell_width)
         self.image = pygame.Surface(sim_size)
-        # create a sprite group for all the particles
+        # create a sprite group for all the particles and a list to track newly added particles
         self.particle_group = pygame.sprite.LayeredDirty()
+        self._new_particles = []
+        # set the background image that will be used when redrawing the sim
+        bgd = None
+        if bg_img is not None:
+            bgd = pygame.transform.smoothscale(bg_img)
+        elif bg_clr is not None:
+            bgd = pygame.Surface(sim_size)
+            bgd.fill(bg_clr)
+        else:
+            print('Using default black background for sim')
+            bgd = pygame.Surface(sim_size)
+            bgd.fill('black')
+        self.particle_group.clear(self.image, bgd)
 
     def _get_abs_pos(self, pos):
         """Get the absolute/pixel position that corresponds to a given grid position.
@@ -49,14 +63,8 @@ class ParticleSim():
             tuple: The clamped grid position.
         """
         x, y = pos
-        if x < 0:
-            x = 0
-        elif x >= self._sim_width:
-            x = self._sim_width
-        if y < 0:
-            y = 0
-        elif y >= self._sim_height:
-            y = self._sim_height
+        x = utils.clamp_int(x, 0, self._sim_width-1)
+        y = utils.clamp_int(y, 0, self._sim_height - 1)
         return (x, y)
 
     def get_cell(self, pos):
@@ -66,34 +74,73 @@ class ParticleSim():
             pos (tuple): The grid position to retrieve the value of.
 
         Returns:
-            any: The particle located at the given grid position, or None if there's no particle
+            pyparticles.properties.BaseParticle: The particle located at the given grid position.
+            None: Returns `None` if there's no particle at the given grid position.
         """
         x, y = pos
         return self._sim_grid[y][x]
 
     def move_particle(self, particle, pos):
-        """_summary_
+        """Moves a particle to a new grid position.
 
         Args:
-            particle (_type_): _description_
-            pos (_type_): _description_
+            particle (pyparticles.properties.BaseParticle): The particle to move.
+            pos (tuple): The grid position to move the particle to.
         """
         old_x, old_y = self.get_pos(particle.rect.topleft)
         self._sim_grid[old_y][old_x] = None
         new_x, new_y = pos
         self._sim_grid[new_y][new_x] = particle
+        particle.rect.topleft = self._get_abs_pos(pos)
 
     def get_pos(self, abs_pos):
+        """Get the grid position that corresponds to a given pixel position.
+
+        Args:
+            abs_pos (tuple): The pixel position to translate to a grid position.
+
+        Returns:
+            tuple: The grid position that corresponds to the given pixel position.
+        """
         x, y = abs_pos
         return (x // self._cell_width, y // self._cell_height)
 
     def update(self, **kwargs):
+        """Update the simulation by one step.
+
+        This updates all the particles in the simulation and redraws the current sim state.
+        Newly created/added particles won't be updated, but they will be drawn. This prevents
+        an 'invisible' first update from occuring. The new particles then have their `dirty`
+        attribute reset, since it doesn't reset automatically for some reason. This prevents
+        them from being stuck for an extra frame.
+
+        Args:
+            **kwargs (any): Variable length list of keyword arguments. These arguments will be
+                passed into each particle's `update()` function.
+        """
         self.particle_group.update(**kwargs, sim=self)
+        self.particle_group.draw(self.image)
+        for p in self._new_particles:
+            p.dirty = 0
+        self._new_particles = []
 
     def add_particle(self, particle, pos):
+        """Add a particle to the simulation at a given grid position.
+
+        Newly added particles are added to a special list that gets cleared after each update.
+
+        Args:
+            particle (pyparticles.properties.BaseParticle): The particle to add.
+            pos (tuple): The grid position to add the particle at.
+
+        Returns:
+            bool: True if the particle was added, False otherwise.
+        """
         x, y = pos
         if self._sim_grid[y][x] is not None:
             return False
         self.particle_group.add(particle)
         self._sim_grid[y][x] = particle
-        particle.topleft = self._get_abs_pos(pos)
+        particle.rect.topleft = self._get_abs_pos(pos)
+        self._new_particles.append(particle)
+        return True
