c870c812f9e67973405c05ee3c2ebc2f4bd9c693
AriaAugs <nataug62@gmail.com>
2024-09-25 14:49:55 -0400

    basic test code

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from pyparticles.particles import types
+from pyparticles.engine import sim as simulation
+import pygame
 
-types.test()
+pygame.init()
+sim = simulation.ParticleSim((30, 30), (12, 12), bg_clr='pink')
+sim.add_particle(types.TestParticle(), (0, 0))
+sim.add_particle(types.TestParticle(), (7, 9))
+sim.add_particle(types.TestParticle(), (15, 5))
+sim.add_particle(types.TestParticle(), (28, 2))
+
+screen = pygame.display.set_mode((600, 600))
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    sim.update()
+    screen.blit(sim.image, (0, 0))
+    pygame.display.flip()
+    clock.tick(2)
