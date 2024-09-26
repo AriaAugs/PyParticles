f9b2f40d32d82ebefa0a23ec292be6328a0437e9
AriaAugs <nataug62@gmail.com>
2024-09-25 22:21:58 -0400

    updated test code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 
 if __name__ == '__main__':
     pygame.init()
-    sim = simulation.ParticleSim((30, 30), (12, 12), bg_clr='pink')
+    sim = simulation.ParticleSim((50, 50), (12, 12), bg_clr='pink')
     sim.add_particle(types.TestParticle(), (0, 0))
     sim.add_particle(types.TestParticle(), (7, 9))
     sim.add_particle(types.TestParticle(), (15, 5))
@@ -15,12 +15,26 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
 
     running = True
+    tick = 0
+    fps = 60
+    tickrate = 0.33 # percent of frames to update sim on
+    tickrate = int(1 / tickrate)
+    adding = False
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        sim.update()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                adding = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                adding = False
+        if adding:
+            pos = sim.get_pos(pygame.mouse.get_pos())
+            sim.add_particle(types.TestParticle(), pos)
+        tick = (tick + 1) % tickrate
+        if tick == 0:
+            sim.update()
         screen.blit(sim.image, (0, 0))
         pygame.display.flip()
-        clock.tick(2)
+        clock.tick(fps)
