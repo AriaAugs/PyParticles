e56f7478526808931d1192e328634adce56d98d5
AriaAugs <nataug62@gmail.com>
2024-10-04 09:25:28 -0400

    profiling program and test functions

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from math import sqrt
 from pyparticles.objects import particles
 from pyparticles.engine import simulation
 from pyparticles.engine.utils import Point
+from random import randint
+import cProfile, pstats, io
+from pstats import SortKey
 
 ### FAR FUTURE PROJECTS ###
 # TODO: Update code to use type hints
@@ -48,7 +51,7 @@ def paint(sim, adding):
             else:
                 sim.remove_particle(pos + off)
 
-if __name__ == '__main__':
+def main():
     pygame.init()
     sim = simulation.ParticleSim((50, 50), (12, 12), bg_clr='pink')
 
@@ -101,3 +104,178 @@ if __name__ == '__main__':
         screen.blit(sim.image, (0, 0))
         pygame.display.flip()
         clock.tick(fps)
+
+def test_draw_perf(pr, dirty=False, moving=False):
+    pygame.init()
+
+    SPRITE_SIZE = 5
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 600
+    MIN_RGB = 50
+    MAX_RGB = 255
+    MAX_LOOPS = 500
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    sprites = None
+    bg_surf = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT)).convert()
+    bg_surf.fill('black')
+
+    if dirty:
+        sprites = pygame.sprite.LayeredDirty()
+    else:
+        sprites = pygame.sprite.Group()
+    for x in range(0, SCREEN_WIDTH, SPRITE_SIZE):
+        for y in range(0, SCREEN_HEIGHT, SPRITE_SIZE):
+            s = None
+            if dirty:
+                s = pygame.sprite.DirtySprite()
+            else:
+                s = pygame.sprite.Sprite()
+            s.image = pygame.Surface((SPRITE_SIZE, SPRITE_SIZE)).convert()
+            s.image.fill(pygame.Color(randint(MIN_RGB, MAX_RGB), randint(MIN_RGB, MAX_RGB), randint(MIN_RGB, MAX_RGB)))
+            s.rect = s.image.get_rect()
+            s.rect.topleft = (x, y)
+            sprites.add(s)
+
+    pr.enable()
+    loops = 0
+    while loops < MAX_LOOPS:
+        # poll to avoid the 'not responding' message
+        pygame.event.poll()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pr.disable()
+                return
+        # drawing
+        if dirty:
+            sprites.draw(screen, bg_surf)
+        else:
+            screen.fill('black')
+            sprites.draw(screen)
+        pygame.display.flip()
+        # move sprites (if enabled)
+        if moving:
+            for s in sprites:
+                x, y = s.rect.topleft
+                x += SPRITE_SIZE
+                if x >= SCREEN_WIDTH:
+                    x = 0
+                    y += SPRITE_SIZE
+                    if y >= SCREEN_HEIGHT:
+                        y = 0
+                s.rect.topleft = (x, y)
+                if dirty:
+                    s.dirty = 1
+        # tick the clock and print the FPS
+        #clock.tick()
+        #print(clock.get_fps())
+        loops += 1
+    pr.disable()
+
+class RandSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.val = randint(0, 1)
+
+    def update(self):
+        self.val = randint(0, 1)
+
+def test_loop_perf(pr, num=8000, max_loops=500, group=False):
+    update_group = pygame.sprite.Group()
+    sprites = None
+    if group:
+        sprites = pygame.sprite.Group()
+    else:
+        sprites = []
+    for x in range(num):
+        s = RandSprite()
+        if group:
+            sprites.add(s)
+        else:
+            sprites.append(s)
+        update_group.add(s)
+    loops = 0
+    pr.enable()
+    while loops < max_loops:
+        l = [s for s in sprites if s.val == 1]
+        #l = []
+        #for s in sprites:
+        #    if s.val == 1:
+        #        l.append(s)
+        update_group.update()
+        loops += 1
+    pr.disable()
+
+class TestClass():
+    def __init__(self):
+        self.val = 0
+
+    def do_a_thing(self):
+        self.val = randint(0, 100)
+        return (self.val % 2) == 1
+
+def test_func_perf(pr, reassign=False, count=100000):
+    if reassign:
+        test_func_perf_other(pr, count)
+        return
+    c = TestClass()
+    loops = 0
+    pr.enable()
+    while loops < count:
+        c.do_a_thing()
+        loops += 1
+    pr.disable()
+
+def test_func_perf_other(pr, count):
+    c = TestClass()
+    f = c.do_a_thing
+    loops = 0
+    pr.enable()
+    while loops < count:
+        f()
+        loops += 1
+    pr.disable()
+
+if __name__ == '__main__':
+    #main()
+
+    func_type = ['Reassigned', 'Dotted']
+    for i in range(2):
+        pr = cProfile.Profile()
+        test_func_perf(pr, reassign=(i==1))
+        st = io.StringIO()
+        sortby = SortKey.CUMULATIVE
+        ps = pstats.Stats(pr, stream=st).sort_stats(sortby)
+        ps.print_stats()
+        print(f'{func_type[i]}')
+        print('--------------------------------------------------')
+        print(st.getvalue())
+    exit(0)
+
+    group_type = ['list', 'group']
+    for i in range(2):
+        pr = cProfile.Profile()
+        test_loop_perf(pr, group=(i==1))
+        st = io.StringIO()
+        sortby = SortKey.CUMULATIVE
+        ps = pstats.Stats(pr, stream=st).sort_stats(sortby)
+        ps.print_stats()
+        print(f'{group_type[i]}')
+        print('--------------------------------------------------')
+        print(st.getvalue())
+    exit(0)
+
+    sprite_type = ['Normal', 'Dirty']
+    movement_type = ['Stationary', 'Moving']
+    for i in range(4):
+        pr = cProfile.Profile()
+        test_draw_perf(pr, dirty=((i%2)==1), moving=((i//2)==1))
+        st = io.StringIO()
+        sortby = SortKey.CUMULATIVE
+        ps = pstats.Stats(pr, stream=st).sort_stats(sortby)
+        ps.print_stats()
+        print(f'{sprite_type[i%2]}, {movement_type[i//2]}')
+        print('--------------------------------------------------')
+        print(st.getvalue())
